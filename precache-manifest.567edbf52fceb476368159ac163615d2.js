a769d2e541a506a0d834411da2b0fcd8c1537bf4
patryk-kilian <patryk.kilian93@gmail.com>
2020-10-08 10:05:57 +0200

    Updates

---
+++ precache-manifest.567edbf52fceb476368159ac163615d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07d5b71aadcfeb5f4b1b11d10e8b90f",
+    "url": "/react-redux-cart/index.html"
+  },
+  {
+    "revision": "edf773ba98cac53f43e3",
+    "url": "/react-redux-cart/static/css/main.42a838db.chunk.css"
+  },
+  {
+    "revision": "99df6cfa959557104b95",
+    "url": "/react-redux-cart/static/js/2.8a3c0d12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-cart/static/js/2.8a3c0d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf773ba98cac53f43e3",
+    "url": "/react-redux-cart/static/js/main.f16538a4.chunk.js"
+  },
+  {
+    "revision": "6715de1583e7ce9dec8b",
+    "url": "/react-redux-cart/static/js/runtime-main.ca328fef.js"
+  }
+]);
